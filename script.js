0f421e1db415c7417d48677a79d64d3e2beb3a92
ayush76671kumar@gmail.com <ayushkumar94703@gmail.com>
2026-04-25 16:06:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+const buttom = document.getElementById("search_button")
+const input = document.getElementById("city")
+const cityname = document.getElementById("city_name");
+const time = document.getElementById("city_time");
+const temp = document.getElementById("city_temp");
+const wind = document.getElementById("wind");
+const feels = document.getElementById("feels");
+
+async function weather(city) {
+    try {
+        const data = await fetch(`http://api.weatherapi.com/v1/current.json?key=e400deae2f8e413d9e4145139252304&q=${city}&aqi=yes`);
+        let res = await data.json();
+        console.log(res);
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+buttom.addEventListener("click", async () => {
+    const value = input.value;
+    const res = await weather(value);
+    document.getElementById("result-card").style.display = "block";
+    if (!value) {
+        cityname.innerHTML = "Enter City Name"
+        time.innerHTML = "";
+        temp.innerHTML = "";
+        feels.innerHTML = "";
+        wind.innerHTML = "";
+        return;
+    }
+    if (res.error) {
+        cityname.innerHTML = "City not found!"
+        time.innerHTML = "";
+        temp.innerHTML = "";
+        feels.innerHTML = "";
+        wind.innerHTML = "";
+        return;
+    }
+    const temp_of_city = res.current.temp_c;
+    cityname.innerHTML = `City:- ${res.location.name}, ${res.location.region}, ${res.location.country}`
+    time.innerHTML = `Date:- ${res.current.last_updated}`
+    temp.innerHTML = `Temp:- ${temp_of_city} °C, ${res.current.condition.text}`
+    feels.innerHTML = `FeelsLike:- ${res.current.feelslike_c} °C`
+    wind.innerHTML = `Wind:- ${res.current.wind_kph} km/h`
+});
+
+
